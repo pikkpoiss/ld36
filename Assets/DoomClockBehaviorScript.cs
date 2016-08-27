3db0b0ade8f63340f77c8ddbb2b0c8511d1e4ad7
Wes <readybreak@gmail.com>
2016-08-26 22:21:29 -0700

    DOOMCLOCK

---
+++ Assets/DoomClockBehaviorScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Yarn.Unity;
+
+public class DoomClockBehaviorScript : MonoBehaviour {
+
+	private int seconds_left = -1;
+
+	[YarnCommand("start")]
+	public void StartClock(string seconds) {
+		seconds_left = int.Parse (seconds);
+	}
+
+	[YarnCommand("stop")]
+	public void StopClock() {
+		seconds_left = -1;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
