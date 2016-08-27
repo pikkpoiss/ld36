1529f68640c6948bb45bf9f6f2df2ebf1655bcd0
Arne Roomann-Kurrik <kurrik@gmail.com>
2016-08-26 21:16:52 -0700

    Initial Yarn Spinner setup

---
+++ Assets/Scripts/DialogueStorage.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogueStorage : VariableStorageBehaviour {
+  Dictionary<string, Yarn.Value> variables = new Dictionary<string, Yarn.Value>();
+
+  [System.Serializable]
+  public class DefaultVariable {
+    public string name;
+    public string value;
+    public Yarn.Value.Type type;
+  }
+    
+  public DefaultVariable[] defaultVariables;
+
+  [Header("Optional debugging tools")]
+  public UnityEngine.UI.Text debugTextView;
+
+  void Awake() {
+    ResetToDefaults();
+  }
+    
+  public override void ResetToDefaults() {
+    Clear();
+    foreach (var variable in defaultVariables) {
+      object value;
+      switch (variable.type) {
+        case Yarn.Value.Type.Number:
+          float f = 0.0f;
+          float.TryParse(variable.value, out f);
+          value = f;
+          break;
+        case Yarn.Value.Type.String:
+          value = variable.value;
+          break;
+        case Yarn.Value.Type.Bool:
+          bool b = false;
+          bool.TryParse(variable.value, out b);
+          value = b;
+          break;
+        case Yarn.Value.Type.Variable:
+          Debug.LogErrorFormat("Can't set variable {0} to {1}: You can't " +
+            "set a default variable to be another variable, because it " +
+            "may not have been initialised yet.", variable.name, variable.value);
+          continue;
+        case Yarn.Value.Type.Null:
+          value = null;
+          break;
+        default:
+          throw new System.ArgumentOutOfRangeException();
+      }
+      var v = new Yarn.Value(value);
+      SetValue("$" + variable.name, v);
+    }
+  }
+    
+  public override void SetValue(string variableName, Yarn.Value value) {
+    variables[variableName] = new Yarn.Value(value);
+  }
+
+  public override Yarn.Value GetValue(string variableName) {
+    if (variables.ContainsKey(variableName) == false) {
+      return Yarn.Value.NULL;
+    }
+    return variables[variableName];
+  }
+    
+  public override void Clear() {
+    variables.Clear();
+  }
+    
+  void Update() {
+    if (debugTextView != null) {
+      var stringBuilder = new System.Text.StringBuilder();
+      foreach (KeyValuePair<string,Yarn.Value> item in variables) {
+        stringBuilder.AppendLine(string.Format("{0} = {1}", 
+          item.Key, 
+          item.Value));
+      }
+      debugTextView.text = stringBuilder.ToString();
+    }
+  }
+}
---
+++ Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUI : Yarn.Unity.DialogueUIBehaviour {
+  public DialogueStorage storage;
+
+  public GameObject dialogueContainer;
+  public GameObject continuePrompt;
+  public Text lineText;
+  public List<Button> optionButtons;
+  public RectTransform gameControlsContainer;
+
+  private Yarn.OptionChooser SetSelectedOption;
+
+  [Tooltip("How quickly to show the text, in seconds per character")]
+  public float textSpeed = 0.025f;
+
+  private void SetDialogueContainerActive(bool value) {
+    if (dialogueContainer != null) {
+      dialogueContainer.SetActive(value);
+    }
+  }
+
+  private void SetContinuePromptActive(bool value) {
+    if (continuePrompt != null) {
+      continuePrompt.SetActive(value);
+    }
+  }
+
+  void Awake() {
+    SetDialogueContainerActive(false);
+    SetContinuePromptActive(false);
+    foreach (var button in optionButtons) {
+      button.gameObject.SetActive (false);
+    }
+    lineText.gameObject.SetActive(false);
+  }
+
+  private string CheckVars(string input) {
+    string output = string.Empty;
+    bool checkingVar = false;
+    string currentVar = string.Empty;
+    int index = 0;
+    while (index < input.Length) {
+      if (input[index] == '[') {
+        checkingVar = true;
+        currentVar = string.Empty;
+      } else if (input[index] == ']') {
+        checkingVar = false;
+        output += ParseVariable(currentVar);
+        currentVar = string.Empty;
+      } else if (checkingVar) {
+        currentVar += input[index];
+      } else {
+        output += input[index];
+      }
+      index += 1;
+    }
+    return output;
+  }
+
+  string ParseVariable(string varName) {
+    if (storage.GetValue(varName) != Yarn.Value.NULL) {
+      return storage.GetValue(varName).AsString;
+    }
+    if (varName == "$time") {
+      return Time.time.ToString();
+    }
+    return varName;
+  }
+
+  public override IEnumerator RunLine(Yarn.Line line) {
+    lineText.gameObject.SetActive(true);
+    var renderedText = CheckVars(line.text);
+    if (textSpeed > 0.0f) {
+      var stringBuilder = new StringBuilder();
+      foreach (char c in renderedText) {
+        stringBuilder.Append(c);
+        lineText.text = stringBuilder.ToString();
+        yield return new WaitForSeconds(textSpeed);
+      }
+    } else {
+      lineText.text = renderedText;
+    }
+    SetContinuePromptActive(true);
+    yield return new WaitForSeconds(2.0f);
+    while (Input.anyKeyDown == false) {
+      yield return null;
+    }
+    lineText.gameObject.SetActive(false);
+    SetContinuePromptActive(false);
+  }
+
+  public override IEnumerator RunOptions(Yarn.Options optionsCollection, Yarn.OptionChooser optionChooser) {
+    if (optionsCollection.options.Count > optionButtons.Count) {
+      Debug.LogWarning("There are more options to present than there are" +
+      "buttons to present them in. This will cause problems.");
+    }
+    int i = 0;
+    Debug.Log("Options Count " + optionsCollection.options.Count);
+    foreach (var optionString in optionsCollection.options) {
+      optionButtons[i].gameObject.SetActive(true);
+      optionButtons[i].GetComponentInChildren<Text>().text = optionString;
+      i++;
+    }
+    SetSelectedOption = optionChooser;
+    while (SetSelectedOption != null) {
+      yield return null;
+    }
+    foreach (var button in optionButtons) {
+      button.gameObject.SetActive(false);
+    }
+  }
+
+  public void SetOption(int selectedOption) {
+    SetSelectedOption(selectedOption);
+    SetSelectedOption = null; 
+  }
+
+  public override IEnumerator RunCommand(Yarn.Command command) {
+    Debug.Log("Command: " + command.text);
+    yield break;
+  }
+
+  public override IEnumerator DialogueStarted() {
+    Debug.Log("Dialogue starting!");
+    SetDialogueContainerActive(true);
+    if (gameControlsContainer != null) {
+      gameControlsContainer.gameObject.SetActive(false);
+    }
+    yield break;
+  }
+
+  public override IEnumerator DialogueComplete() {
+    Debug.Log("Complete!");
+    SetDialogueContainerActive(false);
+    if (gameControlsContainer != null) {
+      gameControlsContainer.gameObject.SetActive(true);
+    }
+    yield break;
+  }
+}
