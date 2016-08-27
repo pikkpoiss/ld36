6f3d4f32cf4d876b5f2969d7781699e05e5a741d
Arne Roomann-Kurrik <kurrik@gmail.com>
2016-08-26 22:05:56 -0700

    Add hover states to the console options

---
+++ Assets/Scripts/ConsoleButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ConsoleButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+  private Text text_;
+  private Color originalColor_;
+  public Color highlightColor;
+
+  private void Awake() {
+    text_ = transform.Find("Text").GetComponent<Text>();
+    originalColor_ = text_.color;
+  }
+
+  public void OnPointerEnter(PointerEventData eventData) {
+    text_.color = highlightColor;
+  }
+
+  public void OnPointerExit(PointerEventData eventData) {
+    text_.color = originalColor_;
+  }
+}
